7177d8cab5bb5bfe472dea2bac10633c50cc84ad
Hem Raj Rana <hemraj.rana@hoolime.co>
2021-07-30 21:19:20 +0530

    added enqueue

--- I/Queue/PriorityQueue/libpqueue/PriorityQueue.c
+++ I/Queue/PriorityQueue/libpqueue/PriorityQueue.c
@@ -1,15 +1,65 @@
 #include <stdlib.h>
 #include "./PriorityQueue.h"
-
 typedef struct _node {
   int v;
   int p;
 } Node;
 
-
 typedef struct _pqueue {
   int size;
   int last;
-  Node* ptr;
+  Node* nodes;
 } PriorityQueue;
 
+
+PriorityQueue* create_pq(int size) {
+  PriorityQueue* ptr = (PriorityQueue*)malloc(sizeof(PriorityQueue));
+  ptr->size = size;
+  ptr->last = -1;
+  ptr->nodes = (Node*)malloc(size * sizeof(Node));
+
+  if(ptr->nodes == NULL) {
+    ptr->size = -1;
+  }
+
+  return ptr;
+}
+
+int pq_is_full(PriorityQueue *ptr) {
+  if(ptr->size == ptr->last) {
+    return 1;
+  }
+
+  return 0;
+}
+
+void bubble_up(PriorityQueue *ptr) {
+  if(ptr->last < 2) {
+    return;
+  }
+
+  for(int i = ptr->last; i > 0; i--) {
+    if(ptr->nodes[ptr->last - 1].p < ptr->nodes[ptr->last].p) {
+      const Node temp = ptr->nodes[ptr->last];
+      ptr->nodes[ptr->last] = ptr->nodes[ptr->last - 1];
+      ptr->nodes[ptr->last - 1] = temp;
+    }
+  }
+}
+
+int pq_enqueue(int num, int p, PriorityQueue *ptr) {
+  Node node = {num, p};
+
+  if(pq_is_full(ptr)){
+    return -1;
+  }
+
+  if(ptr->last == -1) {
+    ptr->last = 0;
+  }
+
+  ptr->nodes[ptr->last] = node;
+  ptr->last++;
+  bubble_up(ptr);
+  return 0;
+}
